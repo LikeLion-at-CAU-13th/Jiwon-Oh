8c781c2e799703365b2579d24c17353d993cda87
ziwonii <nayanako03@gmail.com>
2025-04-06 15:07:52 +0900

    feat: /comments/ , /category 추가

--- Lion13th/posts/urls.py
+++ Lion13th/posts/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', index, name='my-page'),
-    path('<int:id>', get_post_detail)
+    #path('', hello_world, name = 'hello_world'),
+    #path('page', index, name='my-page'),
+    #path('<int:id>', get_post_detail),
+
+    path('', post_list, name="post_list"),
+    path('<int:post_id>/', post_detail, name='post_detail'), # Post 단일 조회
+    path('<int:post_id>/comments/', comment_list, name="comment_list"),
+    path('category/<int:category_id>/', post_category, name="post_category"),
+
 ]
