0760ee1708e8e72c95304bd32105e126e053a427
ziwonii <nayanako03@gmail.com>
2025-05-04 11:23:51 +0900

    feat: comment API CBV

--- Lion13th/posts/urls.py
+++ Lion13th/posts/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     #path('page', index, name='my-page'),
     #path('<int:id>', get_post_detail),
 
-    path('', post_list, name="post_list"),
-    path('<int:post_id>/', post_detail, name='post_detail'), # Post 단일 조회
-    path('<int:post_id>/comments/', comment_list, name="comment_list"),
-    path('category/<int:category_id>/', post_category, name="post_category"),
+    #path('', post_list, name="post_list"),
+    #path('<int:post_id>/', post_detail, name='post_detail'), # Post 단일 조회
+    #path('<int:post_id>/comments/', comment_list, name="comment_list"),
+    #path('category/<int:category_id>/', post_category, name="post_category"),
+    path('', PostList.as_view()),
+    path('<int:post_id>/', PostDetail.as_view()), #꼭 post_id
+    path('comment/<int:post_id>/', CommentList.as_view(), name='comment-list'), #comment_id
 
 ]
